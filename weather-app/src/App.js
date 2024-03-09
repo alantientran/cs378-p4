711ed5482b3f8c2a42e6b114c777246448ce8766
alantientran <att0316698@gmail.com>
2024-03-08 19:28:26 -0600

    smth

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,40 +1,55 @@
-import { useState } from "react";
-import "./App.css";
-
-const api = {
-  key: "83d374be2452e1f5d1e2f2400baf0f42",
-  base: "https://api.openweathermap.org/data/2.5/",
-};
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [search, setSearch] = useState("");
-  const [weather, setWeather] = useState({});
+  const [forecastData, setForecastData] = useState(null);
 
-  const searchRequest = () => {
-    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
-      .then((res) => res.json())
-      .then((result) => {
-        setWeather(result);
-      });
-  };
+  useEffect(() => {
+    const fetchForecast = async () => {
+      try {
+        const response = await fetch(
+          "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const data = await response.json();
+        setForecastData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchForecast();
+  }, []);
 
   return (
-    <div className="App">
-      <div className="App-header">
-        <h1>Hello World</h1>
-        {/* {Search Box} */}
+    <div>
+      <h1>Open Meteo Forecast</h1>
+      {forecastData && (
         <div>
-          <input
-            type="text"
-            placeholder="Search for city"
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <button onClick={searchRequest}>Search</button>
+          <h2>Current</h2>
+          <p>Temperature: {forecastData.current.temperature_2m}°C</p>
+          <p>Wind Speed: {forecastData.current.wind_speed_10m} m/s</p>
+          <h2>Hourly Forecast</h2>
+          <ul>
+            {forecastData.hourly.time.map((time, index) => (
+              <li key={index}>
+                <p>Time: {time}</p>
+                <p>
+                  Temperature: {forecastData.hourly.temperature_2m[index]}°C
+                </p>
+                <p>
+                  Relative Humidity:{" "}
+                  {forecastData.hourly.relative_humidity_2m[index]}%
+                </p>
+                <p>
+                  Wind Speed: {forecastData.hourly.wind_speed_10m[index]} m/s
+                </p>
+              </li>
+            ))}
+          </ul>
         </div>
-        {/* {Location} */}
-        {/* {Temperature F/C} */}
-        {/* {Condition} */}
-      </div>
+      )}
     </div>
   );
 }
